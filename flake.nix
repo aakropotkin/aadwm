348d435bcb45c48de953861dae0967ccf9c639af
Alex Ameen <alex.ameen.tx@gmail.com>
2022-04-27 00:18:17 -0500

    less bullshit in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,42 +1,28 @@
 {
   description = "Alex Ameen's DWM Config";
 
-  inputs.nixpkgs.follows = "nix/nixpkgs";
-
-  outputs = { self, nix, nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       supportedSystems = [ "x86_64-linux" "i686-linux" "aarch64-linux" ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems ( sys: f sys );
       version = "0.1.${nixpkgs.lib.substring 0 8 " +
                 "self.lastModifiedDate}.${self.shortRev or "dirty"}";
     in {
-
-      overlay = final: prev: {
-        aa-dwm = with final;
-          let
-            nix = final.nix;
-          in stdenv.mkDerivation {
+      packages = forAllSystems ( system:
+        let
+          pkgsFor = nixpkgs.legacyPackages.${system};
+        in rec {
+          aa-dwm = pkgsFor.stdenv.mkDerivation {
             name = "aa-dwm";
             src  = self;
-
-            depsTargetTarget = with pkgs.xorg; [ libX11 libXinerama libXft ];
-
+            depsTargetTarget = with pkgsFor.xorg; [libX11 libXinerama libXft];
             prePatch = ''
               sed -i "s@/usr/local@$out@" config.mk
             '';
           };
-      };
-
-      defaultPackage = forAllSystems ( sys: ( import nixpkgs {
-        inherit sys;
-        overlays = [ self.overlay nix.overlay ];
-      } ).aa-dwm );
-
-      nixosModule = { pkgs, ... }: {
-        nixpkgs.overlays = [ self.overlay ];
-      };
-
-      nixosModules.aa-dwm = self.nixosModule;
-
+        default = aa-dwm;
+      } );
+      defaultPackage =
+        forAllSystems ( system: self.packages.${system}.default );
     };
 }
