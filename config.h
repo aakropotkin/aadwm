2ca51f597ee5900b52b58e3621330799cc52ae56
Alex Ameen <alex.ameen.tx@gmail.com>
2022-04-29 23:04:38 -0500

    add browser command

--- config.h
+++ config.h
@@ -57,9 +57,10 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "urxvtc", "-tr", "-sh", "8", NULL };
-static const char *visualcmd[]  = { "my-emacs", NULL };
+static const char * dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char * termcmd[]  = { "urxvtc", "-tr", "-sh", "8", NULL };
+static const char * visualcmd[]  = { "my-emacs", NULL };
+static const char * browsercmd[]  = { "firefox", NULL };
 
 static Key keys[] = {
   /* modifier                     key        function        argument */
@@ -67,6 +68,7 @@ static Key keys[] = {
   { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
   { MODKEY,                       XK_f,      spawn,          {.v = termcmd } },
   { MODKEY,                       XK_v,      spawn,          {.v = visualcmd } },
+  { MODKEY,                       XK_g,      spawn,          {.v = browsercmd } },
   { MODKEY,                       XK_b,      togglebar,      {0} },
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
   { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -125,3 +127,4 @@ static Button buttons[] = {
   { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
+/* vim: set cc= : */
