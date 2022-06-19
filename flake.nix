40ff1c3efe3016dffc4b692da44e356425a200ac
Alex Ameen <alex.ameen.tx@gmail.com>
2022-06-18 20:08:24 -0500

    update lock

--- flake.nix
+++ flake.nix
@@ -4,28 +4,20 @@
   inputs.utils.url = github:numtide/flake-utils;
   inputs.utils.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, utils }:
-    let defaultSystemsMap = utils.lib.eachSystemMap utils.lib.defaultSystems;
+  outputs = { self, nixpkgs, utils }: let
+    inherit (utils.lib) eachDefaultSystemMap;
+  in {
+    packages = eachDefaultSystemMap ( system:
+      let pkgsFor = import nixpkgs { inherit system; };
     in {
-      packages = defaultSystemsMap ( system:
-        let pkgsFor = import nixpkgs { inherit system; };
-        in rec {
-          ak-dwm = pkgsFor.stdenv.mkDerivation {
-            pname = "ak-dwm";
-            version = "0.0.1";
-            src  = self;
-            depsTargetTarget = with pkgsFor.xorg; [
-              libX11 libXinerama libXft
-            ];
-            prePatch = ''
-              sed -i "s,/usr/local,$out," config.mk
-            '';
-          };
-        default = ak-dwm;
-      } );
-    } // ( if ( ( builtins.compareVersions __nixVersion "2.7.0" ) <= 0 )
-           then {
-             defaultPackage =
-               defaultSystemsMap ( system: self.packages.${system}.default );
-           } else {} );
+        ak-dwm = pkgsFor.stdenv.mkDerivation {
+          pname = "ak-dwm";
+          version = "0.0.1";
+          src  = self;
+          depsTargetTarget = with pkgsFor.xorg; [libX11 libXinerama libXft];
+          prePatch = ''sed -i "s,/usr/local,$out," config.mk'';
+        };
+      default = self.packages.${system}.ak-dwm;
+    } );
+  };
 }
