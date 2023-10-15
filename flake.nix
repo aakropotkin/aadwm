0c4c8dc13d47f8321c26e699c24e58bc3d92f22c
Alex Ameen <alex.ameen.tx@gmail.com>
2023-10-15 06:28:40 -0500

    danker flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,64 @@
+# ============================================================================ #
+#
+#
+#
+# ---------------------------------------------------------------------------- #
+
 {
+
+# ---------------------------------------------------------------------------- #
+
   description = "My DWM Config";
 
-  outputs = { self, nixpkgs }: let
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+
+# ---------------------------------------------------------------------------- #
+
+  outputs = { nixpkgs, ... }: let
+
+# ---------------------------------------------------------------------------- #
 
     eachDefaultSystemMap = fn: let
       defaultSystems = ["x86_64-linux" "aarch64-linux" "i686-linux"];
       proc           = name: { inherit name; value = fn name; };
     in builtins.listToAttrs ( map proc defaultSystems );
 
+
+# ---------------------------------------------------------------------------- #
+
+    overlays.deps = final: prev: {};
+
     overlays.ak-dwm = final: prev: {
       ak-dwm = final.stdenv.mkDerivation {
-        pname            = "ak-dwm";
-        version          = "0.0.1";
-        src              = self;
+        pname   = "ak-dwm";
+        version = "0.0.1";
+        src     = builtins.path {
+          path   = ./.;
+          filter = name: type: let
+            bname = baseNameOf name;
+            ext   = let
+              m = builtins.match "[^.]+\\.(.*)" bname;
+            in if m == null then bname else builtins.head m;
+            ignoredFiles = [
+              "flake.nix"
+              "flake.lock"
+              "dwm"
+              "README"
+              "LICENSE"
+              "result"
+              ".ccls-cache"
+              "out"
+            ];
+            ignoredExts     = ["o" "so" "a" "tar" "tar.gz"];
+            ignoredPatterns = ["result-.*" ".*~"];
+            test            = patt: str: ( builtins.match patt str ) != null;
+          in ! (
+            ( builtins.elem bname ignoredFiles ) ||
+            ( builtins.elem ext   ignoredExts ) ||
+            ( builtins.any ( patt: test patt bname ) ignoredPatterns )
+          );
+        };
         depsTargetTarget = [
           final.xorg.libX11
           final.xorg.libXinerama
@@ -21,10 +67,18 @@
         prePatch = ''sed -i "s,/usr/local,$out," config.mk'';
       };
     };
-    overlays.default = overlays.ak-dwm;
 
-    nixosModules.ak-dwm.nixpkgs.overlays = [overlays.ak-dwm];
+    overlays.default = nixpkgs.lib.composeExtensions overlays.deps
+                                                     overlays.ak-dwm;
+
+
+# ---------------------------------------------------------------------------- #
+
+    nixosModules.ak-dwm.nixpkgs.overlays = [overlays.default];
     nixosModules.default                 = nixosModules.ak-dwm;
+
+
+# ---------------------------------------------------------------------------- #
 
     packages = eachDefaultSystemMap ( system: let
       pkgsFor = nixpkgs.legacyPackages.${system}.extend overlays.ak-dwm;
@@ -33,14 +87,26 @@
       default = pkgsFor.ak-dwm;
     } );  # End `packages'
 
+
+# ---------------------------------------------------------------------------- #
+
+  in {
+
+    inherit overlays nixosModules packages;
+
     legacyPackages = eachDefaultSystemMap ( system: let
       pkgsFor = nixpkgs.legacyPackages.${system}.extend overlays.ak-dwm;
     in { inherit (pkgsFor) ak-dwm; } );
 
-  in {
-
-    inherit overlays nixosModules packages legacyPackages;
 
   };  # End `outputs'
 
+
+# ---------------------------------------------------------------------------- #
+
 }
+
+# ---------------------------------------------------------------------------- #
+#
+#
+# ============================================================================ #
